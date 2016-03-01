656cf88d297543667e08e0cf2635807e6b28b142
marcraf <marcraf@gmail.com>
2016-03-01 04:19:20 -0300

    Added files via upload

--- calculadora.js
+++ calculadora.js
@@ -2,11 +2,14 @@ var calculadora= function calc(n1,n2)
 {
 var table=document.getElementById("tabelinha");  
 
+var a1=parseInt(n1);
+var a2=parseInt(n2);
+
   var row = table.insertRow(1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   cell1.innerHTML = n1+" + "+n2;
-  cell2.innerHTML = n1+n2;
+  cell2.innerHTML = (a1+a2);
   
   
   var row=table.insertRow(2);
@@ -14,21 +17,21 @@ var table=document.getElementById("tabelinha");
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   cell1.innerHTML = n1+" - "+n2;
-  cell2.innerHTML = n1-n2;
+  cell2.innerHTML = (parseInt(n1)-parseInt(n2));
   
     var row=table.insertRow(3);
   
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   cell1.innerHTML = n1+" * "+n2;
-  cell2.innerHTML = n1*n2;
+  cell2.innerHTML = (a1*a2);
   
     var row=table.insertRow(4);
   
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   cell1.innerHTML = n1+" / "+n2;
-  cell2.innerHTML = n1/n2;
+  cell2.innerHTML = (a1/a2);
   
   
    var row=table.insertRow(5);
@@ -36,7 +39,7 @@ var table=document.getElementById("tabelinha");
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   cell1.innerHTML = n1+" % "+n2;
-  cell2.innerHTML = n1%n2;
+  cell2.innerHTML = (a1%a2);
   
 };
 
